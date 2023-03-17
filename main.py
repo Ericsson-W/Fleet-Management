2903d226044a91a9fd12de46a5ec57e73a1264ac
Ericsson-W <86052917+Ericsson-W@users.noreply.github.com>
2023-03-17 00:52:15 +0000

    run_live modification

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 from pathlib import Path
 
 from utils.read_configs import ConfigReader
-#from robots.run_live import LiveSimulator
+from robots.run_live import LiveSimulator
 #from simulator.run_scenario import run as run_scenario
 
 while not os.path.isfile('main.py'):
@@ -15,8 +15,8 @@ root_dir = os.getcwd()
 
 params = ConfigReader(root_dir).params
 
-# if params['main']['run_live']:
-#     sim = LiveSimulator(params)
-#     sim.run_game() 
+if params['main']['run_live']:
+    sim = LiveSimulator(params)
+    sim.run_game() 
 # else:
-#     run_scenario(params) # must test with osc as well
+    # run_scenario(params) # must test with osc as well
